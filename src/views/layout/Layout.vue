ad5f6b3fc9b3be70d2968b5ef97da1e2349fe652
desunire <desunire@163.com>
2018-05-04 09:40:03 +0800

    ignore

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="app-wrapper" :class="{hideSidebar:!sidebar.opened}">
-		<!--<sidebar class="sidebar-container"></sidebar>-->
+		<sidebar class="sidebar-container"></sidebar>
 		<div class="main-container">
 			<!--<navbar></navbar>-->
 			<!--<tags-view></tags-view>-->
